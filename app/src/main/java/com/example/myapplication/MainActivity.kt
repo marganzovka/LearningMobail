b530a09563f12074efdcd4d06077d770d4746dad
Evgeni <agenskievgeni@gmail.com>
2023-12-14 04:10:50 +0300

    correcting and adding comments

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,101 +10,96 @@ import androidx.core.view.isVisible
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+
+    // Cоздание объектов
     val markAnswer: MarkAnswer = MarkAnswer(this@MainActivity)
     val learnWordDbHelper: LearnWordDbHelper = LearnWordDbHelper(this@MainActivity, "app", null, 1)
-
-    private lateinit var first: MarkAnswer.LayoutAnswer
-    private lateinit var second: MarkAnswer.LayoutAnswer
-    private lateinit var third: MarkAnswer.LayoutAnswer
-    private lateinit var fourth: MarkAnswer.LayoutAnswer
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        first = MarkAnswer.LayoutAnswer(binding.layoutAnswer1, binding.tvVariantNumber1, binding.tvVariantValue1)
-        second = MarkAnswer.LayoutAnswer(binding.layoutAnswer2, binding.tvVariantNumber2, binding.tvVariantValue2)
-        third = MarkAnswer.LayoutAnswer(binding.layoutAnswer3, binding.tvVariantNumber3, binding.tvVariantValue3)
-        fourth = MarkAnswer.LayoutAnswer(binding.layoutAnswer4, binding.tvVariantNumber4, binding.tvVariantValue4)
+        // Создание объектов (сборка всех нужных View в один объект)
+        var first = MarkAnswer.LayoutAnswer(binding.layoutAnswer1, binding.tvVariantNumber1, binding.tvVariantValue1)
+        var second = MarkAnswer.LayoutAnswer(binding.layoutAnswer2, binding.tvVariantNumber2, binding.tvVariantValue2)
+        var third = MarkAnswer.LayoutAnswer(binding.layoutAnswer3, binding.tvVariantNumber3, binding.tvVariantValue3)
+        var fourth = MarkAnswer.LayoutAnswer(binding.layoutAnswer4, binding.tvVariantNumber4, binding.tvVariantValue4)
 
-
-
-
-
-
+        //Добавление слов в бд
         learnWordDbHelper.AddWords()
-        val count = learnWordDbHelper.getWordCount()
+        // Подсчет слов
+        val count = learnWordDbHelper.getWordCount() //TODO
         println("$count !!!!!!!!!!!!!!!")
 
     // Рандомное слово и расположение его перевода в рандомном месте
 
-        // Выбираем рандомный ID для элемента
+        // Выбор рандомного ID для элемента
         val randWordData = (1..10).random()
 
-        // Получаем этот элемент
+        // Получение рандомного элемента
         val firstWord = learnWordDbHelper.getWordData(randWordData)
-        // Получаем значение использовался ли он
+        // Получаение значение использовался ли он
         var isUsed = firstWord?.isUsed ?: Int
         // Если = 0, то не использовался
         if (isUsed.equals(0)) {
+            // Получение слова и перевода из объекта
             val words = firstWord?.word
             val translate = firstWord?.translate
+            // Вставка выбранного слова в title приложения
             binding.tvQuestionWord.text = words
 
+            //Выбор рандомом места для перевода слова
             val randTranslate = (1..4).random()
 
+            // В зависимости от того, какое место займет перевод
             when (randTranslate) {
                 1 -> {
+                    // Вставка перевода
                     binding.tvVariantValue1.text = translate
-                    FirstVariantCorrect()
+                    // Вызов метода отвечающего за визуальное отображение коректный ли дан ответ (принимает в первую очередь корректную ссылку(кнопку), после, все отсальные. "собранные" View
+                    // передаются в том же порядке, что и ссылки(кнопки))
+                    VariantCorrect(binding.layoutAnswer1, binding.layoutAnswer2, binding.layoutAnswer3, binding.layoutAnswer4, first, second, third, fourth)
+                    //Изменения значения в бд "использовалось ли это слово ранее"
                     learnWordDbHelper.ChangeIsUsed(randTranslate)
 
+                    // Метод выбирающий по еще одному рандомному слову
                     SetTextTranslate(binding.tvVariantValue2)
-
                     SetTextTranslate(binding.tvVariantValue3)
-
                     SetTextTranslate(binding.tvVariantValue4)
 
                 }
 
                 2 -> {
                     binding.tvVariantValue2.text = translate
-                    SecondVariantCorrect()
+                    VariantCorrect(binding.layoutAnswer2, binding.layoutAnswer1, binding.layoutAnswer3, binding.layoutAnswer4, second, first, third, fourth)
                     learnWordDbHelper.ChangeIsUsed(randTranslate)
 
                     SetTextTranslate(binding.tvVariantValue1)
-
                     SetTextTranslate(binding.tvVariantValue3)
-
                     SetTextTranslate(binding.tvVariantValue4)
 
                 }
 
                 3 -> {
                     binding.tvVariantValue3.text = translate
-                    ThirdVariantCorrect()
+                    VariantCorrect(binding.layoutAnswer3, binding.layoutAnswer1, binding.layoutAnswer2, binding.layoutAnswer4, third, first, second, fourth)
                     learnWordDbHelper.ChangeIsUsed(randTranslate)
 
                     SetTextTranslate(binding.tvVariantValue1)
-
                     SetTextTranslate(binding.tvVariantValue2)
-
                     SetTextTranslate(binding.tvVariantValue4)
 
                 }
 
                 4 -> {
                     binding.tvVariantValue4.text = translate
-                    FourthVariantCorrect()
+                    VariantCorrect(binding.layoutAnswer4, binding.layoutAnswer1, binding.layoutAnswer1, binding.layoutAnswer3, fourth, first, second, third)
                     learnWordDbHelper.ChangeIsUsed(randTranslate)
 
                     SetTextTranslate(binding.tvVariantValue1)
-
                     SetTextTranslate(binding.tvVariantValue2)
-
                     SetTextTranslate(binding.tvVariantValue3)
 
                 }
@@ -112,21 +107,18 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        // При нажатии на кнопку все возвращается в исходную позицию
         binding.buttonContinue.setOnClickListener {
 
             markAnswer.markAnswerNetral(first)
-
             markAnswer.markAnswerNetral(second)
-
             markAnswer.markAnswerNetral(third)
-
             markAnswer.markAnswerNetral(fourth)
-
             binding.resultLayout.isVisible = false
         }
 
     }
-    
+
     fun SetTextTranslate(variant: TextView){
         do {
             val randTranslateThird = (1..4).random()
@@ -143,216 +135,51 @@ class MainActivity : AppCompatActivity() {
         }while (usedWord)
     }
 
-    fun FirstVariantCorrect() {
+    fun VariantCorrect(layoutCorrect: LinearLayout, layoutFirstWord: LinearLayout, layoutSecondWord: LinearLayout, layoutThirdWord: LinearLayout,
+                       first: MarkAnswer.LayoutAnswer, second: MarkAnswer.LayoutAnswer, third: MarkAnswer.LayoutAnswer, fourth: MarkAnswer.LayoutAnswer ) {
 
-        binding.layoutAnswer1.setOnClickListener {
+        layoutCorrect.setOnClickListener {
             if (binding.resultLayout.isVisible) {
                 Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
             } else {
                 markAnswer.markAnswerCorrect(first)
-//            }
                 showResultMessage(true)
             }
         }
 
 
-
-        binding.layoutAnswer2.setOnClickListener {
+        layoutFirstWord.setOnClickListener {
             if (binding.resultLayout.isVisible) {
                 Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
             } else {
                 markAnswer.markAnswerWrong(second)
-
                 markAnswer.markAnswerCorrect(first)
-//            }
                 showResultMessage(false)
             }
         }
 
-        binding.layoutAnswer3.setOnClickListener {
+        layoutSecondWord.setOnClickListener {
             if (binding.resultLayout.isVisible) {
                 Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
             } else {
                 markAnswer.markAnswerWrong(third)
-
                 markAnswer.markAnswerCorrect(first)
-//            }
                 showResultMessage(false)
             }
         }
 
-        binding.layoutAnswer4.setOnClickListener {
+        layoutThirdWord.setOnClickListener {
             if (binding.resultLayout.isVisible) {
                 Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
             } else {
                 markAnswer.markAnswerWrong(fourth)
-
                 markAnswer.markAnswerCorrect(first)
-//            }
                 showResultMessage(false)
             }
         }
     }
 
 
-
-    fun SecondVariantCorrect() {
-
-        binding.layoutAnswer1.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(first )
-
-                markAnswer.markAnswerCorrect(second)
-//            }
-                showResultMessage(false)
-            }
-        }
-
-
-
-        binding.layoutAnswer2.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerCorrect(second)
-//            }
-                showResultMessage(true)
-            }
-        }
-
-        binding.layoutAnswer3.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(third)
-
-                markAnswer.markAnswerCorrect(second)
-//            }
-                showResultMessage(false)
-            }
-        }
-
-        binding.layoutAnswer4.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(fourth)
-
-                markAnswer.markAnswerCorrect(second)
-//            }
-                showResultMessage(false)
-            }
-        }
-    }
-
-
-
-    fun ThirdVariantCorrect() {
-
-        binding.layoutAnswer1.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(first)
-
-                markAnswer.markAnswerCorrect(third)
-//            }
-                showResultMessage(false)
-            }
-        }
-
-
-
-        binding.layoutAnswer2.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(second)
-
-                markAnswer.markAnswerCorrect(third)
-//            }
-                showResultMessage(false)
-            }
-        }
-
-        binding.layoutAnswer3.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerCorrect(third )
-//            }
-                showResultMessage(true)
-            }
-        }
-
-        binding.layoutAnswer4.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(fourth)
-
-                markAnswer.markAnswerCorrect(third)
-//            }
-                showResultMessage(false)
-            }
-        }
-    }
-
-
-
-    fun FourthVariantCorrect() {
-
-        binding.layoutAnswer1.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(first)
-
-                markAnswer.markAnswerCorrect(fourth)
-//            }
-                showResultMessage(false)
-            }
-        }
-
-
-
-        binding.layoutAnswer2.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(second)
-
-                markAnswer.markAnswerCorrect(fourth)
-//            }
-                showResultMessage(false)
-            }
-        }
-
-        binding.layoutAnswer3.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerWrong(third)
-
-                markAnswer.markAnswerCorrect(fourth)
-//            }
-                showResultMessage(false)
-            }
-        }
-
-
-        binding.layoutAnswer4.setOnClickListener {
-            if (binding.resultLayout.isVisible) {
-                Toast.makeText(this, "reply accepted", Toast.LENGTH_SHORT).show()
-            } else {
-                markAnswer.markAnswerCorrect(fourth)
-//            }
-                showResultMessage(true)
-            }
-        }
-    }
 
 
     private fun showResultMessage(isCorrect: Boolean) {
